314cd63e0bda7ca48d08256c57094bafe1419d44
Ryan Lambrecht <ryanlambrecht9@gmail.com>
2024-09-24 12:14:16 -0600

    inital commit

---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home_page_view(request):
+    
+    context = {
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greeting": "THAnk you FOR visitING.", 
+    }
+    return render(request, "home.html", context)
+
